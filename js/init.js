97c754abdd45821d27a2b322ebf38f4fa21ca477
ASKoshelenko <18021992alex@gmail.com>
2018-05-20 08:24:11 +0300

    Create Portfolio, Like our works. Stylization

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 $( function() {
     // init tabs
     $( ".services-tab" ).tabs();
-    // init portfolio tabs
+    $( ".portfolio-tab" ).tabs();
 
     //init reviews carousel
     $('.reviews-carousel').slick({
